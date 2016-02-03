7c7285d18fe4a7eb3a91bd02ba7bcadbd1d6e633
florentingarnier <garnier.florentin@gmail.com>
2016-02-03 16:04:57 +0100

    Trying to implement MMM-mode with js2-mode

--- lisp/myMmmConf.el
+++ lisp/myMmmConf.el
@@ -1,6 +1,6 @@
 ;;myMmmConf.el
-(require 'mmm-auto)
+(require 'mmm-mode)
 (setq mmm-global-mode 'maybe)
 (mmm-add-mode-ext-class 'html-mode "\\.php\\'" 'html-php)
-(mmm-add-mode-ext-class 'js2-mode "\\.html\\'" 'html-js)
+(mmm-add-mode-ext-class 'js2-mode "\\.html\\'" 'html-js2)
 (provide 'myMmmConf)
