9b88a945b961f1ab39b5aa38f233134897f67e90
florentingarnier <garnier.florentin@gmail.com>
2016-03-17 14:00:00 +0100

    Indent 2 spaces

--- lisp/myWebModeConf.el
+++ lisp/myWebModeConf.el
@@ -1,3 +1,4 @@
+
 ;;; myWebModeCOnf.el --- Summary
 ;;; Commentary:
 ;;; Code:
@@ -13,6 +14,15 @@
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
 (provide 'myWebModeConf)
 
 ;;; myWebModeCOnf.el Ends here
